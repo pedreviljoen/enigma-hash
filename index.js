415d1187ed1db832d94dbed020a50d634b8284fe
Pedré Viljoen <pedreviljoen@ffmans-MacBook-Pro.local>
2019-01-18 22:04:19 +0200

    Travis CI setup

--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-const crypto = require('crypto')
+const crypto = require("crypto")
 
-function hash(input = '', type = '', code = '') {
-    const algorithms = [
-        'sha256',
-        'sha512'
-    ]
+function hash(input = "", type = "", code = "") {
+  const algorithms = ["sha256", "sha512"]
 
-    const encoding = [
-        'hex',
-        'latin1',
-        'base64'
-    ]
+  const encoding = ["hex", "latin1", "base64"]
 
-    if (algorithms.includes(type)) {
-        const h = crypto.createHash(type)
-        h.update(input)
+  if (algorithms.includes(type)) {
+    const h = crypto.createHash(type)
+    h.update(input)
 
-        return h.digest(encoding[encoding.indexOf(code)] || 'hex')
-    }
-    
-    return type ? 'Algorithm not supported' : 'Algorithm not provided'
+    return h.digest(encoding[encoding.indexOf(code)] || "hex")
+  }
+
+  return type ? "Algorithm not supported" : "Algorithm not provided"
 }
 
-console.log(hash('Hello World', 'sha512', 'hex'))
+console.log(hash("Hello World", "sha512", "hex"))
 
 module.exports = {
-    hash
+  hash
 }
